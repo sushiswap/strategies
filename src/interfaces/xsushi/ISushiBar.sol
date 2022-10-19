4b2e43adb0a5e12462ae956ccaeb81559f7ffa52
Sarang Parikh <sarangparikh22@gmail.com>
2022-10-19 16:23:38 +0400

    init sushi bar strategy

---
+++ src/interfaces/xsushi/ISushiBar.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.10;
+
+interface ISushiBar {
+    function enter(uint256 _amount) external;
+
+    function leave(uint256 _share) external;
+}
