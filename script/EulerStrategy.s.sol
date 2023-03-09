035cf9363546463c67fccdacab51908d8e5f3fbd
Sarang Parikh <sarangparikh22@gmail.com>
2023-03-09 19:04:40 +0400

    updated data for mim

--- script/EulerStrategy.s.sol
+++ script/EulerStrategy.s.sol
@@ -7,9 +7,9 @@ import {EulerStrategy} from "../src/euler/EulerStrategy.sol";
 contract EulerStrategyScript is Script {
     function run() public {
         address euler = 0x27182842E098f60e3D576794A5bFFb0777E025d3;
-        address eToken = 0xc8Bb5b9DF8Dcd2ef92622428843d48fF092C5439;
+        address eToken = 0xcf31501179eef59c8B6722e99489f7035b53F83E;
         address bentoBox = 0xF5BCE5077908a1b7370B9ae04AdC565EBd643966;
-        address token = 0x50D1c9771902476076eCFc8B2A83Ad6b9355a4c9; // FTT
+        address token = 0x99D8a9C45b2ecA8864373A26D1459e3Dff1e17F3; // MIM
         address strategyExecutor = 0xC1056bDFE993340326D2efADaCFDFd6Fab5Eb13c;
         address owner = 0x19B3Eb3Af5D93b77a5619b047De0EED7115A19e7;
         address feeTo = 0x19B3Eb3Af5D93b77a5619b047De0EED7115A19e7;
